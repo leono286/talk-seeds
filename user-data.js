128423b56a90dac475e33003e4ca874f834e6ffe
Leonardo Perez <leperez@google.com>
2021-07-08 18:53:21 -0400

    update

---
+++ user-data.js
@@ -0,0 +1,16 @@
+const data = {
+  gender: "female",
+    name: {
+    first: "Sofia",
+      last: "Lewis"
+  },
+  location: {
+    city: "Masterton",
+      country: "New Zealand",
+  },
+  email: "sofia.lewis@example.com",
+    id: "e770377c-3bb3-4995-ae38-933b2418b617",
+      age: 66,
+        phone: "(215)-731-4667",
+          picture: "https://randomuser.me/api/portraits/women/17.jpg",
+}
